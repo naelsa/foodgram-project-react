dec9a887f76898ff7f8de90f0887eb958a2f1274
naelsa <nhasbutdinov@gmail.com>
2022-11-12 11:58:54 +0500

    isort

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -7,7 +7,8 @@ from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT
+from rest_framework.status import (HTTP_201_CREATED, HTTP_204_NO_CONTENT,
+                                   HTTP_400_BAD_REQUEST)
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from users.models import Subscribe
 
@@ -72,10 +73,11 @@ class CustomUserViewSet(UserViewSet):
                 user=self.request.user, author=following
             )
             serializer = SubscribeSerializer(subscription)
-            return Response(
-                serializer.data,
-                status=HTTP_201_CREATED
-            )
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(
+            {'error': 'Вы не подписаны на этого автора'},
+            status=HTTP_400_BAD_REQUEST
+        )
 
     @subscribe.mapping.delete
     def del_subscribe(self, request, id=None):
